c34dc031e9b2b791546a1fb4d86abcef4a888fb5
shekit <as7790@nyu.edu>
2015-11-03 00:04:31 -0500

    added socket

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var socket_io = require("socket.io")
+
 var app = express();
+
+var io = socket_io();
+app.io = io;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
